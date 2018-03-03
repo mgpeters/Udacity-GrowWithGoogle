b84f356763691d7de6d98f64c73724db06b100ed
Matt Peters <matthew.george.peters@gmail.com>
2018-03-03 13:45:59 -0500

    blew through quizes

---
+++ Lessons/13 - Loops/14 factoral.js
@@ -0,0 +1,11 @@
+/*
+ * Programming Quiz: Factorials (4-7)
+ */
+
+// your code goes here
+var solution = 1;
+
+for(var factoral = 12; factoral >= 1; factoral -= 1){
+    solution = solution * factoral;
+}
+console.log(solution);
