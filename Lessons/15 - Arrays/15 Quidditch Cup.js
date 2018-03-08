608138bcc57448b8a3a33f7ab528ed4ac2248408
Matt Peters <matthew.george.peters@gmail.com>
2018-03-08 13:12:50 -0500

    working array lesson

---
+++ Lessons/15 - Arrays/15 Quidditch Cup.js
@@ -0,0 +1,17 @@
+/*
+ * Programming Quiz: Quidditch Cup (6-5)
+ */
+
+// your code goes here
+
+var team = ["Oliver Wood", "Angelina Johnson", "Katie Bell", "Alicia Spinnet", "George Weasley", "Fred Weasley", "Harry Potter"];
+console.log(hasEnoughPlayers(team));
+
+function hasEnoughPlayers(input){
+	if(input.length >= 7){
+		return true;
+	}else{
+		return false;
+	}
+}
+hasEnoughPlayers(team);
