f25b1cfcae9b32390d479d73b0b6129de03266a3
Matt Peters <matthew.george.peters@gmail.com>
2018-03-23 12:56:01 -0400

    Working quiz

--- Lessons/19 - jQuery/11 - Quiz/Build a DOM Family Tree.js
+++ Lessons/19 - jQuery/11 - Quiz/Build a DOM Family Tree.js
@@ -7,6 +7,10 @@ of '#family2'. '#bruce' should have two <div>s as children, '#madison' and '#hun
 
 // Your code goes here!
 
+
+
+/* Prior Work
+
 var source, family2, bruce, children;
 
 source = $("div:eq(0)");
@@ -15,3 +19,13 @@ bruce = family2.append('<div id="bruce"><h2>Bruce</h2></div>');
 bruce;
 madison = $("<div id='madison'><h3>Madison</h3></div>").insertAfter("#bruce");
 hunter = $("<div id='hunter'><h3>Hunter</h3></div>").insertAfter("#bruce");
+*/
+
+/*
+Step one: Figure out the Parent of id family1
+Step two: append all of the necessary html for “family2” to the Parent
+Step three: now “#family2” exists and you can append the necessary html for "bruce"
+Step four: now “#bruce” exists and you can append the necessary html for "hunter and madison"
+Step five: append madison and hunter as children to “#bruce”`
+*/
+
