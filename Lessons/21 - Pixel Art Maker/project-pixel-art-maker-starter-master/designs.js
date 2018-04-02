8e428ba35c4d9c2fac1c612dbda1f795ee494cfb
Matt Peters <matthew.george.peters@gmail.com>
2018-04-02 18:08:05 -0400

    getting text box inputs

--- Lessons/21 - Pixel Art Maker/project-pixel-art-maker-starter-master/designs.js
+++ Lessons/21 - Pixel Art Maker/project-pixel-art-maker-starter-master/designs.js
@@ -1,10 +1,17 @@
+
+var gridHeight, gridWidth, boxColor;
+
 // Select color input
+$("submit").on('click', function(){
+    gridHeight = $("#inputHeight").val();
+    gridWidth = $("#inputWidth").val();
+
+    console.log("Height is :" + gridHeight + "\nWidth is :" + gridWidth);
+});
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
-
-// Your code goes here!
 
 }
