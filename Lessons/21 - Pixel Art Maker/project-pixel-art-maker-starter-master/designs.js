1e4e0a4f491b7af9fd83b980f253431766ace989
Matt Peters <matthew.george.peters@gmail.com>
2018-04-19 12:30:03 -0400

    re-working pixel art maker

--- Lessons/21 - Pixel Art Maker/project-pixel-art-maker-starter-master/designs.js
+++ Lessons/21 - Pixel Art Maker/project-pixel-art-maker-starter-master/designs.js
@@ -4,18 +4,18 @@ $(document).ready(function(){
 
     function makeGrid() {
         $("#pixelCanvas").append("<div id='gridCell'></div>");
+       
     }
-    // Select color input
+
     $("#button").on('click', function(){
         gridHeight = $("#inputHeight").val();
         gridWidth = $("#inputWidth").val();
-
-        alert("Height is :" + gridHeight + "\nWidth is :" + gridWidth);
         
         makeGrid();
     });
-    // Select size input
-
-    // When size is submitted by the user, call makeGrid()
 
 });
+
+$("#gridCell").on('click', function(){
+    $("#gridCell").html("<div id='gridCell' style='background-color: black'></div>");
+})
