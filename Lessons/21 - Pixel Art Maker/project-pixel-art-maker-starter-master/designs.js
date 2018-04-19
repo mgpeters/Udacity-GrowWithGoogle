808a9f77ac047d998a751240bca0edb6efc30aa7
Matt Peters <matthew.george.peters@gmail.com>
2018-04-19 12:37:42 -0400

    button creates grid cells

--- Lessons/21 - Pixel Art Maker/project-pixel-art-maker-starter-master/designs.js
+++ Lessons/21 - Pixel Art Maker/project-pixel-art-maker-starter-master/designs.js
@@ -3,19 +3,18 @@ $(document).ready(function(){
     var gridHeight, gridWidth, boxColor;
 
     function makeGrid() {
-        $("#pixelCanvas").append("<div id='gridCell'></div>");
+        $(".pixelCanvas").append("<div id='gridCell'></div>");
        
     }
 
     $("#button").on('click', function(){
         gridHeight = $("#inputHeight").val();
-        gridWidth = $("#inputWidth").val();
-        
+        gridWidth = $("#inputWidth").val();  
         makeGrid();
     });
 
 });
 
-$("#gridCell").on('click', function(){
+/*$("#gridCell").on('click', function(){
     $("#gridCell").html("<div id='gridCell' style='background-color: black'></div>");
-})
+})*/
