08d0f3de7a5af359e2c89866905a650ed1f51014
Matt Peters <matthew.george.peters@gmail.com>
2018-05-26 15:03:43 -0400

    3/4 with Syntax lessons

--- Javascript/Lesson 8/Quiz - Template Literal.js
+++ Javascript/Lesson 8/Quiz - Template Literal.js
@@ -14,20 +14,20 @@ const cheetah = {
 
 // creates an animal trading card
 function createAnimalTradingCardHTML(animal) {
-    const cardHTML = '<div class="card">' +
-        '<h3 class="name">' + animal.name + '</h3>' +
-        '<img src="' + animal.name + '.jpg" alt="' + animal.name +'" class="picture">' +
-        '<div class="description">' +
-            '<p class="fact">' + animal.fact + '</p>' +
-            '<ul class="details">' +
-                '<li><span class="bold">Scientific Name</span>: ' + animal.scientificName + '</li>' +
-                '<li><span class="bold">Average Lifespan</span>: ' + animal.lifespan + '</li>' +
-                '<li><span class="bold">Average Speed</span>: ' + animal.speed + '</li>' +
-                '<li><span class="bold">Diet</span>: ' + animal.diet + '</li>' +
-            '</ul>' +
-            '<p class="brief">' + animal.summary + '</p>' +
-        '</div>' +
-    '</div>';
+    const cardHTML = `<div class="card">
+        <h3 class="name"> +${animal.name}</h3>
+        <img src="${animal.name}.jpg" alt="${animal.name}" class="picture">
+        <div class="description">
+            <p class="fact">${animal.fact}</p>
+            <ul class="details">
+                <li><span class="bold">Scientific Name</span>: ${animal.scientificName}</li>
+                <li><span class="bold">Average Lifespan</span>: ${animal.lifespan}</li>
+                <li><span class="bold">Average Speed</span>: ${animal.speed}</li>
+                <li><span class="bold">Diet</span>: ${animal.diet}</li>
+            </ul>
+            <p class="brief">${animal.summary}</p> 
+        </div>
+    </div>`;
 
     return cardHTML;
 }
