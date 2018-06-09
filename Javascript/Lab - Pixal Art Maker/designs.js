f526f426351bb4ee8f0a80d2b8a8376297db5238
Matt Peters <matthew.george.peters@gmail.com>
2018-06-09 19:21:53 -0400

    grabbed color input

--- Javascript/Lab - Pixal Art Maker/designs.js
+++ Javascript/Lab - Pixal Art Maker/designs.js
@@ -1,4 +1,7 @@
 // Select color input
+let color = document.querySelector('#colorPicker').value;
+
+console.log(color);
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
