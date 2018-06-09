0c85946e17924fd4bbd9b966f5daca2f4967ab00
Matt Peters <matthew.george.peters@gmail.com>
2018-06-09 19:14:17 -0400

    started fresh

--- Javascript/Lab - Pixal Art Maker/designs.js
+++ Javascript/Lab - Pixal Art Maker/designs.js
@@ -1,26 +1,9 @@
+// Select color input
+// Select size input
 
-$(document).ready(function(){
-    var gridHeight, gridWidth, boxColor,
-        cell = "<div id='gridCell'></div>";
+// When size is submitted by the user, call makeGrid()
 
-    function makeGrid() {
-        for(let x = 0; x < gridWidth; x += 1){
-            for(let y = 0; y < gridHeight; y += 1){
-                $(".pixelCanvas").append(cell);
-            }
-        }
-        $(".pixelCanvas").width(960 / gridWidth);
-        $(".pixelCanvas").height(960 / gridHeight);
-    }
-
-    $("#button").on('click', function(){
-        gridHeight = $("#inputHeight").val();
-        gridWidth = $("#inputWidth").val();  
-        makeGrid();
-    });
-
-});
-
-/*$("#gridCell").on('click', function(){
-    $("#gridCell").html("<div id='gridCell' style='background-color: black'></div>");
-})*/
+function makeGrid() {
+    // Your code goes here!
+  }
+  
