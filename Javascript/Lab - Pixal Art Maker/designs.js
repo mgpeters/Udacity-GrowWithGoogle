9574a47fc4e70dfe688998e2fe5c9e0a70a82b97
Matt Peters <matthew.george.peters@gmail.com>
2018-06-09 20:10:35 -0400

    working for loops makeGrid()

--- Javascript/Lab - Pixal Art Maker/designs.js
+++ Javascript/Lab - Pixal Art Maker/designs.js
@@ -2,6 +2,8 @@
 
 let pixelCanvas = document.getElementById('pixelCanvas'),
     submitButton = document.getElementById('submitButton'),
+    divCell = '<div class="divCell"></div>',
+    br = '</br>',
     color,
     width,
     height;
@@ -16,13 +18,27 @@ let pixelCanvas = document.getElementById('pixelCanvas'),
     function grabWidthAndHeight(){
     height = document.querySelector("#inputHeight").value,
     width = document.querySelector("#inputWidth").value;
+    console.log(`WORKED! - Height: ${height} Width: ${width}`);
     }    
 
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
     // Your code goes here!
-    pixelCanvas.insertAdjacentHTML('afterbegin', '<div class="pixelContainerDiv"></div>');
+    pixelCanvas.insertAdjacentHTML('afterbegin', '<div id="pixelContainerDiv"></div>');
+
+    // Possibly not needed - let pixelContainerDiv = document.getElementById('pixelContainerDiv');
+
+    grabWidthAndHeight();
+
+    for (let i = 0; i < height; i += 1){
+        for(let x = 0; x < width; x += 1){
+            pixelCanvas.insertAdjacentHTML('afterbegin', divCell);
+        }
+        pixelCanvas.insertAdjacentHTML('beforeend', br);
+    }
   }
 
-  submitButton.addEventListener('click', makeGrid);
+  //submitButton.addEventListener('click', makeGrid);
+
+  makeGrid();
