9b33914371afc3a349475f25345eff0d5d2ef4ca
Matt Peters <matthew.george.peters@gmail.com>
2018-06-10 13:17:58 -0400

    created cleargrid fx

--- Javascript/Lab - Pixal Art Maker/designs.js
+++ Javascript/Lab - Pixal Art Maker/designs.js
@@ -9,33 +9,37 @@ let color,
     
 
 // Select color input
-    function grabColor(){
-        color = document.querySelector('#colorPicker').value;
-    }
+function grabColor(){
+    color = document.querySelector('#colorPicker').value;
+}
 
 // Function to fill cells with selected color
 
-    function colorFill(){
-        this.setAttribute('style', `background-color: ${color}`);
-    }
+function colorFill(){
+    this.setAttribute('style', `background-color: ${color}`);
+}
 
 // Select size input
-    function grabWidthAndHeight(){
+function grabWidthAndHeight(){
     height = document.querySelector("#inputHeight").value,
     width = document.querySelector("#inputWidth").value;
     console.log(`WORKED! - Height: ${height} Width: ${width}`);
-    }    
+}    
+
+//Clears grid
+
+function clearGrid(){
+    while(pixelCanvas.firstChild){
+        pixelCanvas.removeChild(pixelCanvas.firstChild);
+    }
+}
 
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
     // Your code goes here!
-    //Possibly Not Needed - pixelCanvas.insertAdjacentHTML('afterbegin', '<div id="pixelContainerDiv"></div>');
-
-    let containerDiv = document.createElement('div');    
-
-    // Possibly not needed - let pixelContainerDiv = document.getElementById('pixelContainerDiv');
-
+    clearGrid();
+    grabColor();
     grabWidthAndHeight();
 
     for (let i = 0; i < height; i += 1){
