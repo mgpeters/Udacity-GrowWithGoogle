995da3e5fefc75a19a28f095cac8197a7788b734
Matt Peters <matthew.george.peters@gmail.com>
2018-08-04 14:31:36 -0400

    cleared up global namespace

--- cat-clicker/script/script.js
+++ cat-clicker/script/script.js
@@ -1,16 +1,27 @@
-const   main = document.getElementById('main'),
-        catDisplay = document.getElementById('cat-display'),
-        catList = document.getElementById('cat-chart__list');
-
-let catArray = [tabby = new Animal('tabby', '../img/cat01.jpeg'),
+global = {
+    catArray: [tabby = new Animal('tabby', '../img/cat01.jpeg'),
                 isla = new Animal('isla', '../img/cat02.jpg'),
                 mittins = new Animal('mittins', '../img/cat03.jpg'),
                 blue = new Animal('blue', '../img/cat04.jpeg'),
                 red = new Animal('red', '../img/cat05.jpg')
                 ],
-    global = {
-        prior: undefined
-    };
+    catDisplay: document.getElementById('cat-display'),
+    catList: document.getElementById('cat-chart__list'),
+    prior: undefined,
+    toggle_visibility: function toggle_visibility(input) { // https://css-tricks.com/snippets/javascript/showhide-element/ 08/03/18
+                                let idLi = document.getElementById(input);
+    
+                                if(global.prior != undefined){
+                                    global.prior.style.visibility = 'hidden';
+                                }
+                        
+                                if(idLi.style.visibility == 'hidden'){
+                                    idLi.style.visibility = 'visible';
+                                }else{
+                                    idLi.style.visibility = 'hidden';
+                                }
+                                global.prior = idLi;}
+};
 
 function Animal(name, photoPath){
     this.counter = 0,
@@ -26,53 +37,39 @@ function Animal(name, photoPath){
                             <img class="cat-photo-main" src="${this.photoPath}" alt="cat-photo">
                         </div>`
 }
-function toggle_visibility(input) { // https://css-tricks.com/snippets/javascript/showhide-element/ 08/03/18
-    let idLi = document.getElementById(input);
 
-        if(global.prior != undefined){
-            global.prior.style.visibility = 'hidden';
-        }
-
-        if(idLi.style.visibility == 'hidden'){
-            idLi.style.visibility = 'visible';
-        }else{
-            idLi.style.visibility = 'hidden';
-        }
-        global.prior = idLi;
-
-}
 //Display list of Cats
-for(let x = 0; x < catArray.length; x += 1){
+for(let x = 0; x < global.catArray.length; x += 1){
     let z = document.createElement('li');
     z.className = `cat-chart__list__li`;
-    z.innerHTML = catArray[x].name;
-    catList.appendChild(z);
+    z.innerHTML = global.catArray[x].name;
+    global.catList.appendChild(z);
 
     let catListIndividual = document.getElementsByClassName('cat-chart__list__li');
     
     catListIndividual[x].addEventListener('click', function(event){
         let name = catListIndividual[x].innerHTML;
 
-        toggle_visibility(name);
+        global.toggle_visibility(name);
     })
 
 }
 
 // Display Cat Photos
-for(let x = 0; x < catArray.length; x += 1){
+for(let x = 0; x < global.catArray.length; x += 1){
     let z = document.createElement('div');
     z.className = `cat-box`;
-    z.id = `${catArray[x].name}`;
+    z.id = `${global.catArray[x].name}`;
     z.setAttribute('style', 'visibility: hidden');
-    z.innerHTML = catArray[x].htmlInject;
-    catDisplay.appendChild(z);
+    z.innerHTML = global.catArray[x].htmlInject;
+    global.catDisplay.appendChild(z);
 
-let catPhoto = document.querySelectorAll('img'),
-    catCounter = document.getElementsByClassName('counter__number');
+    let catPhoto = document.querySelectorAll('img'),
+        catCounter = document.getElementsByClassName('counter__number');
 
-    catPhoto[x].addEventListener('click', function(){
-        //the element has been clicked... do stuff here
-          catArray[x].counter += 1;
-          catCounter[x].innerHTML = catArray[x].counter;
-      }, false);
+        catPhoto[x].addEventListener('click', function(){
+            //the element has been clicked... do stuff here
+            global.catArray[x].counter += 1;
+            catCounter[x].innerHTML = global.catArray[x].counter;
+        }, false);
 }
