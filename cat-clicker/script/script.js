ed31486d6147778a9c3857d1046ffc6c79bfb0c5
Matt Peters <matthew.george.peters@gmail.com>
2018-08-10 16:14:43 -0400

    refractored in vars in iffy

--- cat-clicker/script/script.js
+++ cat-clicker/script/script.js
@@ -1,4 +1,4 @@
-global = {
+/*global = {
     catArray: [tabby = new Animal('tabby', '../img/cat01.jpeg'),
                 isla = new Animal('isla', '../img/cat02.jpg'),
                 mittins = new Animal('mittins', '../img/cat03.jpg'),
@@ -72,18 +72,18 @@ for(let x = 0; x < global.catArray.length; x += 1){
             global.catArray[x].counter += 1;
             catCounter[x].innerHTML = global.catArray[x].counter;
         }, false);
-}
+}*/
 //mvc implamentation
 
 (function(){
 
     var data = {
         catArray: [tabby = new Animal('tabby', '../img/cat01.jpeg'),
-        isla = new Animal('isla', '../img/cat02.jpg'),
-        mittins = new Animal('mittins', '../img/cat03.jpg'),
-        blue = new Animal('blue', '../img/cat04.jpeg'),
-        red = new Animal('red', '../img/cat05.jpg')
-        ],
+                    isla = new Animal('isla', '../img/cat02.jpg'),
+                    mittins = new Animal('mittins', '../img/cat03.jpg'),
+                    blue = new Animal('blue', '../img/cat04.jpeg'),
+                    red = new Animal('red', '../img/cat05.jpg')
+                    ],
         catDisplay: document.getElementById('cat-display'),
         catList: document.getElementById('cat-chart__list'),
         prior: undefined,
@@ -100,45 +100,59 @@ for(let x = 0; x < global.catArray.length; x += 1){
                                     idLi.style.visibility = 'hidden';
                                 }
                                 global.prior = idLi;}
-
+        Animal: function Animal(name, photoPath){
+                    this.counter = 0,
+                    this.name = name,
+                    this.photoPath = photoPath,
+                    this.htmlInject = ` <div class="counter">
+                                    <span class="counter__number">0</span>
+                                </div>
+                                <div class="cat-name">
+                                    <span class="cat-name__inner">${this.name}</span>
+                                </div>
+                                <div class="main-picture">
+                                    <img class="cat-photo-main" src="${this.photoPath}" alt="cat-photo">
+                                </div>`
+        }
     };
 
     var = octopus {
-        
-    }
+
+    };
 
     var view = {
-        for(let x = 0; x < global.catArray.length; x += 1){
-            let z = document.createElement('li');
-            z.className = `cat-chart__list__li`;
-            z.innerHTML = global.catArray[x].name;
-            global.catList.appendChild(z);
-        
-            let catListIndividual = document.getElementsByClassName('cat-chart__list__li');
-            
-            catListIndividual[x].addEventListener('click', function(event){
-                let name = catListIndividual[x].innerHTML;
-        
-                global.toggle_visibility(name);
-            })
-        }
-        for(let x = 0; x < global.catArray.length; x += 1){
-            let z = document.createElement('div');
-            z.className = `cat-box`;
-            z.id = `${global.catArray[x].name}`;
-            z.setAttribute('style', 'visibility: hidden');
-            z.innerHTML = global.catArray[x].htmlInject;
-            global.catDisplay.appendChild(z);
-        
-            let catPhoto = document.querySelectorAll('img'),
-                catCounter = document.getElementsByClassName('counter__number');
-        
-                catPhoto[x].addEventListener('click', function(){
-                    //the element has been clicked... do stuff here
-                    global.catArray[x].counter += 1;
-                    catCounter[x].innerHTML = global.catArray[x].counter;
-                }, false);
-        }
-    }
-
+        //Display list of Cats
+        catListDisplay: for(let x = 0; x < global.catArray.length; x += 1){
+                        let z = document.createElement('li');
+                        z.className = `cat-chart__list__li`;
+                        z.innerHTML = global.catArray[x].name;
+                        global.catList.appendChild(z);
+                    
+                        let catListIndividual = document.getElementsByClassName('cat-chart__list__li');
+                        
+                        catListIndividual[x].addEventListener('click', function(event){
+                            let name = catListIndividual[x].innerHTML;
+                    
+                            global.toggle_visibility(name);
+                        })
+                        },
+        // Display Cat Photos
+        catPhotoDisplay: for(let x = 0; x < global.catArray.length; x += 1){
+                            let z = document.createElement('div');
+                            z.className = `cat-box`;
+                            z.id = `${global.catArray[x].name}`;
+                            z.setAttribute('style', 'visibility: hidden');
+                            z.innerHTML = global.catArray[x].htmlInject;
+                            global.catDisplay.appendChild(z);
+                        
+                            let catPhoto = document.querySelectorAll('img'),
+                                catCounter = document.getElementsByClassName('counter__number');
+                        
+                                catPhoto[x].addEventListener('click', function(){
+                                    //the element has been clicked... do stuff here
+                                    global.catArray[x].counter += 1;
+                                    catCounter[x].innerHTML = global.catArray[x].counter;
+                                }, false);
+                        }
+    };
 })();
