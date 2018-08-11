7d83d6d21f417c02e3c067bc8abfb72b79165ae0
Matt Peters <matthew.george.peters@gmail.com>
2018-08-11 12:38:42 -0400

    updated paths from global to data

--- cat-clicker/script/script.js
+++ cat-clicker/script/script.js
@@ -78,7 +78,7 @@ for(let x = 0; x < global.catArray.length; x += 1){
 (function(){
 
     var data = {
-        catArray: [tabby = new Animal('tabby', '../img/cat01.jpeg'),
+        catArray: [ tabby = new Animal('tabby', '../img/cat01.jpeg'),
                     isla = new Animal('isla', '../img/cat02.jpg'),
                     mittins = new Animal('mittins', '../img/cat03.jpg'),
                     blue = new Animal('blue', '../img/cat04.jpeg'),
@@ -103,7 +103,7 @@ for(let x = 0; x < global.catArray.length; x += 1){
         }
     };
 
-    var = octopus {
+    var octopus = {
         init: function(){
             view.init();
         }
@@ -111,35 +111,35 @@ for(let x = 0; x < global.catArray.length; x += 1){
 
     var view = {
         init: function(){         //Display list of Cats
-                        for(let x = 0; x < global.catArray.length; x += 1){
-                        let z = document.createElement('li');
-                        z.className = `cat-chart__list__li`;
-                        z.innerHTML = global.catArray[x].name;
-                        global.catList.appendChild(z);
-                    
-                        let catListIndividual = document.getElementsByClassName('cat-chart__list__li');
+                        for(let x = 0; x < data.catArray.length; x += 1){
+                            let z = document.createElement('li');
+                            z.className = `cat-chart__list__li`;
+                            z.innerHTML = data.catArray[x].name;
+                            data.catList.appendChild(z);
                         
-                        catListIndividual[x].addEventListener('click', function(event){
-                            let name = catListIndividual[x].innerHTML;
+                            let catListIndividual = document.getElementsByClassName('cat-chart__list__li');
+                            
+                            catListIndividual[x].addEventListener('click', function(event){
+                                let name = catListIndividual[x].innerHTML;
                     
-                            global.toggle_visibility(name);
+                            view.toggle_visibility(name);
                             })
                         }
                                 // Display Cat Photos
-                        for(let x = 0; x < global.catArray.length; x += 1){
+                        for(let x = 0; x < data.catArray.length; x += 1){
                             let z = document.createElement('div');
                             z.className = `cat-box`;
-                            z.id = `${global.catArray[x].name}`;
+                            z.id = `${data.catArray[x].name}`;
                             z.setAttribute('style', 'visibility: hidden');
-                            z.innerHTML = global.catArray[x].htmlInject;
-                            global.catDisplay.appendChild(z);
+                            z.innerHTML = data.catArray[x].htmlInject;
+                            data.catDisplay.appendChild(z);
                         
                             let catPhoto = document.querySelectorAll('img'),
                                 catCounter = document.getElementsByClassName('counter__number');
                         
                                 catPhoto[x].addEventListener('click', function(){
                                     //the element has been clicked... do stuff here
-                                    global.catArray[x].counter += 1;
+                                    data.catArray[x].counter += 1;
                                     catCounter[x].innerHTML = global.catArray[x].counter;
                                 }, false);
                         }
@@ -148,8 +148,8 @@ for(let x = 0; x < global.catArray.length; x += 1){
         toggle_visibility: function(input) { // https://css-tricks.com/snippets/javascript/showhide-element/ 08/03/18
         let idLi = document.getElementById(input);
 
-        if(global.prior != undefined){
-            global.prior.style.visibility = 'hidden';
+        if(data.prior != undefined){
+            data.prior.style.visibility = 'hidden';
         }
 
         if(idLi.style.visibility == 'hidden'){
@@ -157,6 +157,6 @@ for(let x = 0; x < global.catArray.length; x += 1){
         }else{
             idLi.style.visibility = 'hidden';
         }
-        global.prior = idLi;
+        data.prior = idLi;
         }
 })();
